0b1e032d0faa11dbb10e54399d9d8398ef110edd
Sheldon Stevens <sgste080@yahoo.com>
2021-05-03 18:37:02 -0700

    fuck it

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('tech_post', 'root', 'root', {
-      host: 'localhost',
-      port: 3306,
-      dialect: 'mysql'
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
     });
-
+}
 module.exports = sequelize;
