7013f9903467ab7daf25c5a7ce879a7db2a9a162
Sheldon Stevens <sgste080@yahoo.com>
2021-07-03 16:42:53 -0700

    newnew

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./user');
 const Post = require('./post');
-const Comment = require('./Comment');
+const Comment = require('./comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
